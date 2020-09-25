78a711b3c892d682c0b26e802ba22c00a8c8330a
Telugabbai <mekalasreenivasarao8@gmail.com>
2020-09-25 21:49:17 +0530

    upto date 25-09-20

--- src/main/scala/com/spark/sql/WorldDevIndicatorsAnalysis.scala
+++ src/main/scala/com/spark/sql/WorldDevIndicatorsAnalysis.scala
@@ -12,6 +12,27 @@ object WorldDevIndicatorsAnalysis {
     import spark.implicits._
     import spark.sql
 
+    // Data Source . Datasets link given in README.md
+    val country_data="C:\\Users\\Sreenivas\\Documents\\Project\\World development\\data\\Country.csv"
+    val Indicator_data="C:\\Users\\Sreenivas\\Documents\\Project\\World development\\data\\Indicators.csv"
+
+    // Dataframes creation
+    val CountryDf=spark.read.format("csv")
+        .option("header","true").option("inferSchema","true").option("path",country_data).load()
+
+    val IndicatorDf=spark.read.format("csv").option("header","true")
+        .option("inferSchema","true").option("path",Indicator_data).load()
+    // creating Views
+
+    CountryDf.createOrReplaceTempView("Country")
+    IndicatorDf.createOrReplaceTempView("Indicators")
+
+    // Problem statements
+    //GINI index of China,India,Japan
+
+    val china=spark.sql("select Year,Value as China_GINI_value from Indicators where IndicatorCode='SI.POV.GINI' and CountryCode='CHN' order by Year asc")
+    china.show()
+
     spark.stop()
   }
 }
